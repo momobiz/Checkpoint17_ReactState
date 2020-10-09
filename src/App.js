07c0669dd44cdb5195037b4b40eca36cc1994e00
momobiz <mohamedaliannabi35@gmail.com>
2020-10-09 11:51:00 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,15 +14,15 @@ class App extends React.Component {
                imgSrc:visage}, 
         show:false,
         count:0
+       
       };
   stateAlter=()=>this.setState({show:!this.state.show})
-    
-
   
   render(){
-
+    const {count}=this.state;
   
     return (
+    
       <div> 
          <h1> Fiche de description </h1>
          <Button variant="primary" onClick={this.stateAlter} className="bouton">Afficher </Button>
@@ -30,23 +30,20 @@ class App extends React.Component {
        {this.state.show? ( <Person fullName={this.state.person.fullName} bio={this.state.person.bio}
              imgSrc={this.state.person.imgSrc} profession={this.state.person.profession}/>) :(null)}
      
-      
+      <h2> Time : {count} </h2>
       </div>
       
 
     )
    
 }
+
 componentDidMount=()=>{
-  this.time=setInterval(()=>this.setState({count:this.state.count+1}),
-  1000)
-  console.log(this.time)
-  
-  
+ this.time=setInterval(()=>this.setState({count:this.state.count+1}),1000);
   
 }
 componentWillUnmount=()=>{
-  clearInterval(this.time);
+ clearInterval(this.time);
 }
 
 
